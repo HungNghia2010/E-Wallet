fe6d2b6ffa1281f115f887af225d405715a4b112
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2022-05-19 12:36:24 +0700

    update

--- js/controllers/auth.js
+++ js/controllers/auth.js
@@ -1,21 +1,31 @@
+const mysql = require('mysql')
 
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 exports.register = (req, res) => {
     console.log(req.body)
 
-    //const { name, birth, email, phone, cmnd, address} = req.body
+    const { name, birth, email, phone, cmnd, address} = req.body
+    // if(!name){
+    //     return res.render('register',{
+    //         message: 'Vui lòng nhập tên'
+    //     })
+    // }else {
+    //     db.query('SELECT email FROM register WHERE email = ?', [email], (error, result) => {
+    //         if(error){
+    //             console.log(error)
+    //         }
+    //         if(result.length > 0){
+    //             return res.render('register', {
+    //                 message: 'Email này đã tồn tại'
+    //             })
+    //         }
+    //     })
+    // }
 
-    // db.query('SELECT email FROM register WHERE email = ?', [email], (error, result) => {
-    //     if(error){
-    //         console.log(error)
-    //     }
-    //     if(result.length > 0){
-    //         return res.render('register', {
-    //             message: 'Email này đã tồn tại'
-    //         })
-    //     }
-    // })
-
-
-    res.send("Form submitted")
 }
