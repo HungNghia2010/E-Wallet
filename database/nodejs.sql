c0cf3d278a36b883f03e0d54b52e82ad63877bbd
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2022-05-19 13:16:31 +0700

    update

--- database/nodejs.sql
+++ database/nodejs.sql
@@ -9,8 +9,8 @@ CREATE TABLE register(
     email varchar(64) COLLATE utf8_unicode_ci NOT NULL,
     phone_number varchar(15) NOT NULL,
     identity varchar(20) NOT NULL,
-    birth date,
+    birth varchar(25) NOT NULL,
     address varchar(255) NOT NULL,
-    CMND1 varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    CMND2 varchar(255) COLLATE utf8_unicode_ci NOT NULL 
+    CMND1 varchar(255) COLLATE utf8_unicode_ci ,
+    CMND2 varchar(255) COLLATE utf8_unicode_ci  
 );
