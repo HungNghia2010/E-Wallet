b7f3309fe70de8b82937a4d758f7f4d2f634562a
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2022-05-26 21:31:51 +0700

    update all

--- js/public/js/infochangepass.js
+++ js/public/js/infochangepass.js
@@ -1,34 +1,8 @@
-document.getElementById('form-changepass').addEventListener("submit", ()=>{
-    const pass = {
-        pwd: pwd.value,
-        pwdnew: pwdnew.value,
-        pwdcf: pwdcf.value
 
-    }
-    fetch("/auth/changepassword",{
-        method: "POST",
-        body: JSON.stringify(pass),
-        headers: {
-            "Content-type":"application/json"
-        }
-    }).then(res => res.json())
-    .then(data => {
-        if(data.status == "error"){
-            dialogCont.style.display = "none"
-            errorMessage.style.display = "block"
-            error.innerText = data.error
-        }else{
-            dialogCont.style.display = "block"
-            errorMessage.style.display = "none"
-            success.innerText = data.success 
-        }
-    })
-})
-
-// document.getElementById('close').onclick = function(){
-//     dialogCont.style.display = "none"
-//     window.location.reload()
-// }
+document.getElementById('close').onclick = function(){
+    dialogCont.style.display = "none"
+    window.location.reload()
+}
 
 $('#eye').click(function() {
     $(this).toggleClass('open');
