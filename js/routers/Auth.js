b82827f1431d308eb1d0721cdf833dc2448aec77
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2022-05-27 13:54:17 +0700

    update

--- js/routers/Auth.js
+++ js/routers/Auth.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const authController = require('../controllers/auth');
 const loggedIn = require('../controllers/LoggedIn')
+const multer = require('multer')
+const fsExtra = require('fs-extra')
+
 
 const Router = express.Router();
 
-Router.post('/register', authController.register)
+const fileStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let path = '../public/images';
+        if (!fsExtra.existsSync(path)) {
+            fsExtra.mkdirSync(path)
+        }
+
+        cb(null,path)
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+
+const upload = multer({ storage: fileStorage});
+
+Router.post('/register', upload.single('images') , authController.register)
 Router.post('/login', authController.login)
 Router.post('/forgot', authController.checkmail)
 Router.post('/firststep', loggedIn.loggedIn, authController.change_passft)
