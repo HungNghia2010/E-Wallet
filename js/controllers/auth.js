71d581dbcdac05a38a56a97db2225399b334b6a7
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2022-05-31 00:48:20 +0700

    update

--- js/controllers/auth.js
+++ js/controllers/auth.js
@@ -824,15 +824,14 @@ exports.muatheviettel = async (req, res) => {
 
 >>>>>>> Stashed changes
 //random string for password
-const generateRandomString = (myLength) => {
-    const chars =
-      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+function generateRandomString(myLength) {
+    const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
     const randomArray = Array.from(
-      { length: myLength },
-      (v, k) => chars[Math.floor(Math.random() * chars.length)]
+        { length: myLength },
+        (v, k) => chars[Math.floor(Math.random() * chars.length)]
     );
-  
+
     const randomString = randomArray.join("");
     return randomString;
-};
+}
 
